52eb76f0593dc4c5a8ab2aa4b8d6a3941bb38741
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-03 00:28:50 +0800

    добавил логику аутентификации

--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
     template_name = 'crm/home.html'
-    context = {
-        'title': 'CRM-Система',
-        'text': 'just a text'
-    }
-    return render(request, template_name, context)
+    if request.method == 'POST':
+        username = request.POST['login']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            message = 'Вы успешно вошли в систему'
+            messages.success(request, message)
+            return redirect('home')
+        else:
+            message = 'Ошибка входа'
+            messages.error(request, message)
+            return redirect('home')
+    else:
+        return render(request, template_name)
+
+
+def logout_user(request):
+    pass
