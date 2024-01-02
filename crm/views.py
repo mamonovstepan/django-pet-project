d8a2c601d00f3239dcbe029a203620e0e4f978cc
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-02 15:22:37 +0800

    создал контроллер для стартовой страницы

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    template_name = 'crm/home.html'
+    context = {
+        'title': 'CRM-Система',
+        'text': 'just a text'
+    }
+    return render(request, template_name, context)
