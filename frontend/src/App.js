3b380c39913d2b6b4091ccebf0083e11218616d2
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-02-07 06:00:24 +0800

    написан минимальный функционал отображения данных

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,34 @@
-import './App.css';
+import axios from 'axios'
+import React from 'react';
 
-function App() {
-  
+class App extends React.Component {
+    state = {details: [],}
+    
+    componentDidMount() {
+        let data;
+        axios.get('http://localhost:8000/crm-api/')
+        .then(res => {
+            data = res.data;
+            this.setState({
+                details: data
+            });
+        })
+    .catch(err => { })
+    }
+    render() {
+        return(
+            <div>
+            <header><h1>Данные полученные из Django</h1></header>
+            <hr></hr>
+            {this.state.details.map((output, id) => (
+                <div>
+                    <h1>{output.first_name} {output.last_name}</h1>
+                    <h1></h1>
+                </div>
+            ))}
+            </div>
+        )
+    }    
 }
 
 export default App;
