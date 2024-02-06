faf2a048cc228613c5814f99ed774ea43cd79175
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-02-06 11:15:40 +0800

    написаны представления для api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from crm.models import Lead
+from .serializers import LeadSerializer
 
-# Create your views here.
+
+class LeadList(generics.ListAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
+
+
+class LeadDetail(generics.RetrieveAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
