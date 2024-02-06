d836b978bc4703c53c305ce58dd63c55fabbfb2f
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-02-06 11:13:13 +0800

    настроены пути для APi

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apis.urls')),
+    path('api/', include('api.urls')),
     path('', include('crm.urls')),
 ]
